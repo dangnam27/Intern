6d552a475d399486bbe82328dabbc085af02c6d7
Đăn Nam <dangnam97yt@gmail.com>
2023-05-08 16:12:01 +0700

    first commit

---
+++ src/component/Navbar.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+
+
+export default function Navbar() {
+  return (
+<div>
+    <nav class="navbar navbar-expand-lg bg-primary  ">
+        <div class="container">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a aria-current="page" href="#">
+            <i class="fa-solid fa-house"></i></a>
+        </li>
+        <li class="nav-item ">
+          <a class="nav-link " href="../Introduce.jsx">GIỚI THIỆU</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">GẠCH LÁT NỀN</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">GẠCH ỐP TƯỜNG</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">GẠCH TRANG TRÍ</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">TIN TỨC</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">LIÊN HỆ</a>
+        </li>
+        
+      </ul>
+      <div className="buttons">
+        <a href="#" className=" btn btn-outline-dark" color='#fffff'>
+            KHUYỄN MÃI
+        </a>
+      </div>
+    </div>
+  </div>
+</nav>
+    </div>
+  )
+}
---
+++ src/component/Products.jsx
@@ -0,0 +1,128 @@
+import React,{useState, useEffect} from 'react';
+import './Products.css' ;
+
+
+const Products = () =>{
+
+    const [data, setdata] = useState([]);
+    const [filter, setfilter] = useState(data);
+    const [loading, setloading] = useState(false);
+  
+
+    return(
+        <div>
+                
+
+        <div class="row ">
+            <a href="#"><h2>Gạch lát nền khuyến mãi</h2></a>
+            
+                <div class="  col-md-2 ">
+                    <a href="#"> <div class="card" >
+                    <img class="card-img-top d-block w-100 " src="https://gachonline.vn/images/san-pham/product/resized/3001_240x240.jpg" alt="Card image cap"/>
+                    <div class="card-body">
+                    <h5 class="card-title">Gach lát nền Vitto - 5612</h5>
+                    <p class="text-danger">155.000 ₫/m2 </p>
+                    </div>
+                </div>
+                </a> 
+                </div>
+            
+        
+                <div class="  col-md-2">
+                    <div class="card" >
+                    <img class="card-img-top d-block w-100" src="https://gachonline.vn/images/san-pham/product/resized/5613_240x240.jpg" alt="Card image cap"/>
+                    <div class="card-body">
+                        <h5 class="card-title">Gach lát nền Vitto - 5613</h5>
+                        <p class="text-danger">155.000 ₫/m2 </p>
+
+                    </div>
+                </div>
+            </div>
+                <div class="  col-md-2">
+                    <div class="card" >
+                        <img class="card-img-top d-block w-100" src="https://gachonline.vn/images/san-pham/product/resized/5612_240x240.jpg" alt="Card image cap"/>
+                        <div class="card-body">
+                        <h5 class="card-title">Gạch lát nền VITTO VT3001</h5>
+                        <p class="text-danger">155.000 ₫/m2 </p>
+                        </div>
+                </div>
+            </div>
+                <div class="  col-md-2">
+                    <div class="card" >
+                        <img class="card-img-top d-block w-100" src="https://gachonline.vn/images/san-pham/product/resized/5612_240x240.jpg" alt="Card image cap"/>
+                        <div class="card-body">
+                        <h5 class="card-title">Gạch lát nền VITTO VT3001</h5>
+                        <p class="text-danger">155.000 ₫/m2 </p>
+                        </div>
+                </div>
+            </div>
+                <div class="  col-md-2">
+                    <div class="card" >
+                        <img class="card-img-top d-block w-100" src="https://gachonline.vn/images/san-pham/product/resized/5612_240x240.jpg" alt="Card image cap"/>
+                        <div class="card-body">
+                        <h5 class="card-title">Gạch lát nền VITTO VT3001</h5>
+                        <p class="text-danger">155.000 ₫/m2 </p>
+                        </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row ">
+            <a href="#"><h2>Gạch ốp tường khuyến mãi</h2></a>
+                <div class="  col-md-2 ">
+                
+                <div class="card" >
+                    <img class="card-img-top d-block w-100 " src="https://gachonline.vn/images/san-pham/product/resized/3001_240x240.jpg" alt="Card image cap"/>
+                    <div class="card-body">
+                    <h5 class="card-title">Gach lát nền Vitto - 5612</h5>
+                    <p class="text-danger">155.000 ₫/m2 </p>
+                    </div>
+                </div>   
+                </div>
+            
+        
+                <div class="  col-md-2">
+                    <div class="card" >
+                    <img class="card-img-top d-block w-100" src="https://gachonline.vn/images/san-pham/product/resized/5613_240x240.jpg" alt="Card image cap"/>
+                    <div class="card-body">
+                        <h5 class="card-title">Gach lát nền Vitto - 5613</h5>
+                        <p class="text-danger">155.000 ₫/m2 </p>
+
+                    </div>
+                </div>
+            </div>
+                <div class="  col-md-2">
+                    <div class="card" >
+                        <img class="card-img-top d-block w-100" src="https://gachonline.vn/images/san-pham/product/resized/5612_240x240.jpg" alt="Card image cap"/>
+                        <div class="card-body">
+                        <h5 class="card-title">Gạch lát nền VITTO VT3001</h5>
+                        <p class="text-danger">155.000 ₫/m2 </p>
+                        </div>
+                </div>
+            </div>
+                <div class="  col-md-2">
+                    <div class="card" >
+                        <img class="card-img-top d-block w-100" src="https://gachonline.vn/images/san-pham/product/resized/5612_240x240.jpg" alt="Card image cap"/>
+                        <div class="card-body">
+                        <h5 class="card-title">Gạch lát nền VITTO VT3001</h5>
+                        <p class="text-danger">155.000 ₫/m2 </p>
+                        </div>
+                </div>
+            </div>
+                <div class="  col-md-2">
+                    <div class="card" >
+                        <img class="card-img-top d-block w-100" src="https://gachonline.vn/images/san-pham/product/resized/5612_240x240.jpg" alt="Card image cap"/>
+                        <div class="card-body">
+                        <h5 class="card-title">Gạch lát nền VITTO VT3001</h5>
+                        <p class="text-danger">155.000 ₫/m2 </p>
+                        </div>
+                </div>
+            </div>
+        </div>
+        </div>
+        
+    );
+
+}
+export default Products;
+
